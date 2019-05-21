8c6dd88895803e4421724c97617cda86286ce65b
Brice Waegeneire <brice.wge@gmail.com>
2019-05-21 14:07:06 +0200

    inital commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+#
+# Deploy a new version of rojer-skyline-1
+# TODO Use ssh to achieve this
---
+++ provision.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+#
+# Create a new VM running rojer-skyline-1
+# TODO Use VBoxManage to acheive this
